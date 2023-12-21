5c9a891521f1a03d7c3cd414974a5c80b7863ff2
Ruslan Golovinsky <ruslangolovinsky@mytona.com>
2023-12-21 16:30:22 +0400

    More fun for playground!

--- scripts/utilities/Teleport.gd
+++ scripts/utilities/Teleport.gd
@@ -1,6 +1,18 @@
+@tool
 class_name Teleport extends Node2D
 
-@export var destination: Vector2
+@export var apply_velocity: Vector2 = Vector2.ZERO
+@export var destination: Marker2D = null
 
 func teleport(body: Node2D):
-	body.position = destination
+	if not body.is_in_group("teleportable"): return
+
+	assert(destination != null, "Teleport: destination is null")
+	body.global_position = destination.global_position
+	if apply_velocity:
+		body.velocity = apply_velocity
+
+func _draw() -> void:
+	if Engine.is_editor_hint() and destination != null:
+		draw_dashed_line(Vector2.ZERO, destination.position, Color.GREEN_YELLOW)
+
